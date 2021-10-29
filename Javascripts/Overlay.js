25089e0fcc054c605915ec6bed0911a9a273e3e8
RONIT SINGH <ronitsingh7003@gmail.com>
2021-10-29 20:28:09 +0530

    Added Overlays to Admin, Teacher and Student

---
+++ Javascripts/Overlay.js
@@ -0,0 +1,34 @@
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal-container");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal-container");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
+
+document.addEventListener('keydown', function(e){
+  if(e.key === 'Escape') {
+      const modalss = document.querySelectorAll(".modal-container");
+      modalss.forEach((modal) => modal.classList.remove("active"));
+      overlay.classList.remove("active");
+  }
+})
