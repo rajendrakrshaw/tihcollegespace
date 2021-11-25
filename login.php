0c7bd2ffc397f0d5a6016299d222c0a519223c02
rajendrakrshaw <rajendra250601@gmail.com>
2021-11-25 22:43:42 +0530

    Added login System

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "tihcollespace");
+
+if(isset($_POST['submit'])){
+    $uid = $_POST['uid'];
+    $pass = $_POST['password'];
+    $q = "SELECT * FROM `login` WHERE uid = '$uid' and password = '$pass'";
+    $query = mysqli_query($conn, $q);
+
+    if(mysqli_num_rows($query) > 0){
+        $_SESSION['uid'] = $uid;
+        while($row = mysqli_fetch_array($query)){
+            $role = $row['role'];
+        }
+        if($role == "admin"){
+            header("location:users/Admin/index.html");
+        }
+        elseif($role == "teacher"){
+            header("location:users/Teacher/index.php");
+        }
+        elseif($role == "student"){
+            header("location:users/Student/index.html");
+        }
+        
+    }
+    else{
+        echo "Invalid Credential";
+    }
+}
+
+?>
